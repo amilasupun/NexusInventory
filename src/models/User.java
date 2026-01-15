55d6b99a7e66b2c64fc04718a94c59d2925364d4
amilasupun <dexteramilasupun@gmail.com>
2026-01-15 15:41:57 +0530

    Add User model with properties and accessors - member 1 (@amilasupun)

---
+++ src/models/User.java
@@ -0,0 +1,49 @@
+package models;
+
+public class User {
+    int id;
+    String username;
+    String password;
+    String role;
+    String fullName;
+    
+    public int getId() {
+        return id;
+    }
+    
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public String getUsername() {
+        return username;
+    }
+    
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
+    public String getPassword() {
+        return password;
+    }
+    
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    public String getRole() {
+        return role;
+    }
+    
+    public void setRole(String role) {
+        this.role = role;
+    }
+    
+    public String getFullName() {
+        return fullName;
+    }
+    
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+}
